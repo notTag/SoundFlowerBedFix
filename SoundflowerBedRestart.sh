90f86991c6450d8d4203889bda177bb0aea62246
Nick Tag <nick.tagliasacchi@gmail.com>
2017-07-04 22:30:05 -0400

    Added a third party library to switch my audio source if the SoundFlower (2ch) is not selected (which is what it needs to be set to for the audio control to work.

--- SoundflowerBedRestart.sh
+++ SoundflowerBedRestart.sh
@@ -1,5 +1,19 @@
-SwitchAudioSource -s "Soundflower (2ch)"
+# SwitchAudioSource -s "Soundflower (2ch)"
 
+audSrc=`SwitchAudioSource -c`
+echo $audSrc
+if [ "$audSrc" != "Soundflower (2ch)" ]
+then
+	echo "Attempting to switch to Soundflower"
+	SwitchAudioSource -s "Soundflower (2ch)"
+	SwitchAudioSource -c
+	echo "Attempting to switch to Soundflower"
+else
+	echo "failed"
+fi
+
+
+#pgrep -f "SoundflowerBed"
 echo kill
 kill $(pgrep -f "SoundflowerBed" | awk '{print $1}')
 echo open
